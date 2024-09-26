abb01d9d1fc64cee7e7003c2e0a59de85554708d
unknown <amazighmalek.com>
2024-09-26 19:44:10 +0200

    zod and Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Button } from "@/components/ui/button";
 import { FolderPlus } from 'lucide-react';
 import {
@@ -6,16 +7,62 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { getProductActions } from "@/actions/todoActions";
+// import { getProductActions } from "@/actions/todoActions";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Textarea } from "@/components/ui/textarea";
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+const productFormSchema = z.object({
+  title: z
+    .string()
+    .min(2, {
+      message: "Username must be at least 2 characters.",
+    })
+    .max(30, {
+      message: "Username must not be longer than 30 characters.",
+    }),
+  
+  body: z
+  .string()
+  .max(100, {
+    message: "Username must not be longer than 30 characters.",
+  }).optional(),
+})
+
+type ProductFormValues = z.infer<typeof productFormSchema>
 
 
-export default async function  Home() {
-  const products = await getProductActions()
+export default  function  Home() {
+  // const products = await getProductActions();
+
+    // This can come from your database or API.
+const defaultValues: Partial<ProductFormValues> = {
+  title:"DEFAULT TITLE",
+  body: "DEFAULT BODY",
+}
+
+  const form = useForm<ProductFormValues>({
+    resolver: zodResolver(productFormSchema),
+    defaultValues,
+    mode: "onChange",
+  })
+
+
+
+const onSubmit = () =>{}
+
   return (
    <main>
-
-   
     
      <div 
      className="flex justify-between items-center p-8 pb-4 gap-6 sm:p-20 font-[family-name:var(--font-geist-sans)]"
@@ -33,20 +80,56 @@ export default async function  Home() {
             Make changes to your profile here. Click save when your done.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
+        <div className=" py-4">
+           
+        <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name. It can be your real name or a
+                pseudonym. You can only change this once every 30 days.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      
+        <FormField
+          control={form.control}
+          name="body"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Bio</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Tell us a little bit about yourself"
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                You can <span>@mention</span> other users and organizations to
+                link to them.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+       
+        <Button type="submit">Update profile</Button>
+      </form>
+    </Form>
+         
         </div>
+
         <DialogFooter>
           <Button type="submit">Save</Button>
         </DialogFooter>
@@ -56,13 +139,13 @@ export default async function  Home() {
    
 
     </div> 
-     <div className="grid grid-cols-8 gap-4 mx-5">
+     {/* <div className="grid grid-cols-8 gap-4 mx-5">
       {products && products.map(product => (
        <li key={product.id}>
       {product.title}
        </li>
        ))}
-      </div>
+      </div> */}
     </main>
    
   );
